5dd87fe6419a9371a412601d8630bd52e318e35f
wenceslaob <yellowpostman@gmail.com>
2019-06-06 12:20:15 +0300

    added table at homepage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @include('layouts.head')
+        <title>Recruit Me - Home page</title>
+{{--        <title>App Name - @yield('title')</title>--}}
+</head>
+<body>
+{{--@section('sidebar')--}}
+{{--    This is the master sidebar.--}}
+{{--@show--}}
+
+{{--<div class="container">--}}
+{{--    @yield('content')--}}
+{{--</div>--}}
+<table class="table table-hover">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">ID</th>
+            <th scope="col">NAME</th>
+            <th scope="col">COMPANY</th>
+            <th scope="col">COUNTRY</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">PHONE</th>
+            <th scope="col">LAST DOWNLOADED</th>
+        </tr>
+
+    </thead>
+
+    <tbody>
+
+
+    </tbody>
+
+</table>
+</body>
+</html>
